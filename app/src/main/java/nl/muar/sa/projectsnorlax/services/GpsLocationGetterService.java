32a0c23010d7b35d07e22001e454615013bdc5d9
[Trevell] <tpaul.test@gmail.com>
2017-06-15 11:48:27 +0100

    manifest file and GPS service added

--- app/src/main/java/nl/muar/sa/projectsnorlax/services/GpsLocationGetterService.java
+++ app/src/main/java/nl/muar/sa/projectsnorlax/services/GpsLocationGetterService.java
@@ -1,20 +1,63 @@
 package nl.muar.sa.projectsnorlax.services;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+
+import com.google.android.gms.location.LocationListener;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+
+import static android.location.LocationManager.*;
+
 
 /**
  * Created by android-eng on 15/06/17.
  */
 
-public class GpsLocationGetterService extends Service{
+public class GpsLocationGetterService extends Service {
+
+    LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        LocationManager locationManager = getSystemService(Context.LOCATION_SERVICE);
-    }
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //do something
+
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return TODO;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (android.location.LocationListener) locationListener);
+    return null;
 }
+
