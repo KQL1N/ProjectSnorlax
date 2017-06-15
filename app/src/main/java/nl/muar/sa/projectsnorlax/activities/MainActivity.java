97cc856c04625474b254a23209629f106caeb07d
Android Engineer <android-eng@baesystems.com>
2017-06-15 16:59:22 +0100

    Added UI and basic functionality. About to begin swipe-handling

--- app/src/main/java/nl/muar/sa/projectsnorlax/activities/MainActivity.java
+++ app/src/main/java/nl/muar/sa/projectsnorlax/activities/MainActivity.java
@@ -1,26 +1,33 @@
 package nl.muar.sa.projectsnorlax.activities;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import nl.muar.sa.projectsnorlax.util.UserPreferenceManager;
+import sa.muar.nl.projectsnorlax.R;
+
+import static nl.muar.sa.projectsnorlax.util.UserPreferenceManager.LAST_LOCATION;
 import static nl.muar.sa.projectsnorlax.util.UserPreferenceManager.PREFERENCE_MODE;
 import static nl.muar.sa.projectsnorlax.util.UserPreferenceManager.PREFERRED_LOCATION;
-import static nl.muar.sa.projectsnorlax.util.UserPreferenceManager.LAST_LOCATION;
-import sa.muar.nl.projectsnorlax.R;
-import static android.view.View.OnTouchListener;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -35,6 +42,11 @@ public class MainActivity extends AppCompatActivity
     public TextView currentDayText;
     public TextView closingTimeText;
     public String[] days;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private String latitudeLabel;
+    private String longitudeLabel;
+    private Location lastLocation;
+    private int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -48,6 +60,19 @@ public class MainActivity extends AppCompatActivity
         loadCorrectLocation();
         currentLocationText.setText(currentLocation);
         days = getDates();
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            getLocation();
+        }
     }
 
     @Override
@@ -90,9 +115,13 @@ public class MainActivity extends AppCompatActivity
 
             case R.id.defaultgps:
                 Log.i(TAG, "Use GPS Menu Item Clicked");
-                savePreferenceMode(GPS_MODE);
+
+                mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+                getLocation();
+
+                //savePreferenceMode(GPS_MODE);
                 // TODO; LOAD FROM GPS
-                printStuff();
+                //printStuff();
                 return true;
 
             case R.id.defaultguildford:
@@ -117,6 +146,46 @@ public class MainActivity extends AppCompatActivity
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @SuppressWarnings("MissingPermission")
+    private void getLocation() {
+        mFusedLocationClient.getLastLocation()
+                .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(Task<Location> task) {
+                        String log="the task is not succesful";
+                        if(task.isSuccessful()==true){
+                            log = "The task is successful";
+                        }
+                        Log.w(TAG, log);
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            lastLocation = task.getResult();
+
+                                    double a = lastLocation.getLatitude();
+
+                                    lastLocation.getLongitude();
+                            Log.w(TAG, "getLastLocation worked" + a);
+                        } else {
+                            Log.w(TAG, "getLastLocation:exception "+ task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void compareDistance(Location location) {
+        double num = location.getLatitude();
+        String test = location.toString();
+        int duration = Toast.LENGTH_SHORT;
+        Log.d(TAG, "Got distances "+ num + test);
+        Context context = getApplicationContext();
+        CharSequence helpText =""+num+"";
+        Toast toast = Toast.makeText(context, helpText, duration);
+        toast.show();
+
+        Toast toast2 = Toast.makeText(context, test, duration);
+        toast2.show();
+    }
+
 
     public void printStuff()
     {
