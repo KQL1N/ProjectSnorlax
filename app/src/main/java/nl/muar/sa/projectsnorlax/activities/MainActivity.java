3593b06d16e3b1a20327718edd0f63e7031be9de
[Trevell] <tpaul.test@gmail.com>
2017-06-15 16:23:34 +0100

    can get the gps location

--- app/src/main/java/nl/muar/sa/projectsnorlax/activities/MainActivity.java
+++ app/src/main/java/nl/muar/sa/projectsnorlax/activities/MainActivity.java
@@ -1,21 +1,37 @@
 package nl.muar.sa.projectsnorlax.activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+
 
 import sa.muar.nl.projectsnorlax.R;
 
 public class MainActivity extends AppCompatActivity
 {
     public static final String TAG = "Main Activity";
+    private FusedLocationProviderClient mFusedLocationClient;
+    private String latitudeLabel;
+    private String longitudeLabel;
+    private Location lastLocation;
+    private int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,6 +39,19 @@ public class MainActivity extends AppCompatActivity
         Log.i(TAG, "Creating Activity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            getLocation();
+        }
     }
 
     @Override
@@ -41,10 +70,52 @@ public class MainActivity extends AppCompatActivity
         {
             case R.id.menugloucester:
                 Log.i(TAG, "Gloucester Menu Item Clicked");
+                mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+                getLocation();
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @SuppressWarnings("MissingPermission")
+    private void getLocation() {
+        mFusedLocationClient.getLastLocation()
+                .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(Task<Location> task) {
+                        String log="the task is not succesful";
+                        if(task.isSuccessful()==true){
+                            log = "The task is successful";
+                        }
+                        Log.w(TAG, log);
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            lastLocation = task.getResult();
+
+                                    double a = lastLocation.getLatitude();
+
+                                    lastLocation.getLongitude();
+                            Log.w(TAG, "getLastLocation worked" + a);
+                        } else {
+                            Log.w(TAG, "getLastLocation:exception "+ task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void compareDistance(Location location) {
+        double num = location.getLatitude();
+        String test = location.toString();
+        int duration = Toast.LENGTH_SHORT;
+        Log.d(TAG, "Got distances "+ num + test);
+        Context context = getApplicationContext();
+        CharSequence helpText =""+num+"";
+        Toast toast = Toast.makeText(context, helpText, duration);
+        toast.show();
+
+        Toast toast2 = Toast.makeText(context, test, duration);
+        toast2.show();
+    }
+
 }
