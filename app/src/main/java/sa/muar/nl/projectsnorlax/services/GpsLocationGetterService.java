75646be003563a08ba5cfbb49639cb01e0c32247
[Trevell] <tpaul.test@gmail.com>
2017-06-15 10:10:59 +0100

    started GPS local Getter service

--- app/src/main/java/sa/muar/nl/projectsnorlax/services/GpsLocationGetterService.java
+++ app/src/main/java/sa/muar/nl/projectsnorlax/services/GpsLocationGetterService.java
@@ -1,8 +1,22 @@
 package sa.muar.nl.projectsnorlax.services;
 
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.lang.reflect.GenericArrayType;
+
 /**
  * Created by android-eng on 15/06/17.
  */
 
-public class GpsLocationGetterService {
+public class GpsLocationGetterService extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        LocationManager locationManager = getSystemService(Context.LOCATION_SERVICE);
+    }
 }
