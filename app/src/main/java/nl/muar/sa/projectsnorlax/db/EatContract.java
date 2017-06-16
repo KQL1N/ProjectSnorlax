20e0f7519bfb49a9be31bbf041b7788861837fac
hcevans <hannah.c.evans@live.co.uk>
2017-06-16 11:02:17 +0100

    Snorlax-11-Create-DB-Helper-Class
    Contract class amended

--- app/src/main/java/nl/muar/sa/projectsnorlax/db/EatContract.java
+++ app/src/main/java/nl/muar/sa/projectsnorlax/db/EatContract.java
@@ -1,26 +1,32 @@
 package nl.muar.sa.projectsnorlax.db;
 
 import android.provider.BaseColumns;
+import android.util.Log;
 
-public class eatContract {
+/**
+ * Created by HCEvans on 15/06/17.
+ */
 
-    private eatContract(){}
+public class EatContract {
 
-    public static class Location implements BaseColumns{
-        public static final String TABLE_NAME = "location";
+    private EatContract(){}
+
+    public static class Restaurant implements BaseColumns{
+        public static final String TABLE_NAME = "restaurants";
         public static final String COLUMN_NAME_NAME = "name";
         public static final String COLUMN_NAME_LONGITUDE = "longitude";
         public static final String COLUMN_NAME_LATITUDE = "latitude";
-        public static final String COLUMN_NAME_OPENINGTIME = "openingTime";
-        public static final String COLUMN_NAME_CLOSINGTIME = "closingTime";
+        public static final String COLUMN_NAME_OPENING_TIME = "opening_time";
+        public static final String COLUMN_NAME_CLOSING_TIME = "closing_time";
     }
 
     public static class MenuItem implements BaseColumns{
-        public static final String TABLE_NAME = "menuItem";
+        public static final String TABLE_NAME = "menu_item";
         public static final String COLUMN_NAME_NAME = "name";
         public static final String COLUMN_NAME_DESCRIPTION = "description";
         public static final String COLUMN_NAME_PRICE = "price";
         public static final String COLUMN_NAME_SECTION= "section";
         public static final String COLUMN_NAME_DATE = "date";
+        public static final String COLUMN_NAME_RESTARAUNT_ID = "restaurant_ID";
     }
 }
